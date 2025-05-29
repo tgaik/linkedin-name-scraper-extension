a6d709d7126c6f808d4b8ea34b91792d41a253f6
tgaik <gks.tej@gmail.com>
2025-05-28 21:08:01 -0400

    sanitize

--- setup_git_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git init
-git add .
-git commit -m "init"
-git branch -M main
-git remote add origin https://github.com/tgaik/linkedin-name-scraper-extension.git
-git push -u origin main
